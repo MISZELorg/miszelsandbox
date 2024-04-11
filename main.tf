9f0934f00a2bbef0e8c9e1efd0478b1a928877bb
Kamil Miszel <kamil.miszel@nordcloud.com>
2024-04-11 15:39:26 +0200

    1st_module

--- main.tf
+++ main.tf
@@ -1,27 +1,5 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = ">= 3.8.0"
-    }
-  }
-
-  backend "azurerm" {
-    resource_group_name  = "rg-githubtfstates"
-    storage_account_name = "miszelsandbox"
-    container_name       = "tfstate"
-    key                  = "terraform.tfstate"
-    use_oidc             = true
-    subscription_id      = "f80611eb-0851-4373-b7a3-f272906843c4"
-    tenant_id            = "48c383d8-47c5-48f9-9e8b-afe4f2519054"
-  }
+module "resource_group_rg1" {
+  source              = "./resource_group"
+  resource_group_name = var.resource_group_names["rg1"]
+  location            = var.location
 }
-
-provider "azurerm" {
-  features {}
-  use_oidc = true
-}
-
-
-
-
