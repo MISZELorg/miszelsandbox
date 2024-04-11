d44e4ffa3611dcd392fab3dcd311d42ca233fadc
Kamil Miszel <kamil.miszel@nordcloud.com>
2024-04-11 11:15:20 +0200

    foreach

--- variables.tf
+++ variables.tf
@@ -1,2 +1,22 @@
 # variable "location" {}
 # variable "resource_group_name" {}
+variable "rg_map" {
+  type = map(object({
+    name     = string
+    location = string
+  }))
+  default = {
+    "rg1" = {
+      name     = "rg1"
+      location = "westeurope"
+    }
+    "rg2" = {
+      name     = "rg2"
+      location = "northeurope"
+    }
+    "rg3" = {
+      name     = "rg3"
+      location = "polandcentral"
+    }
+  }
+}
