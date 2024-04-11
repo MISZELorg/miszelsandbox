f0d16570a2c83f5c94a4fed0ae09d3b983da51e8
Kamil Miszel <kamil.miszel@nordcloud.com>
2024-04-11 12:23:58 +0200

    remtestvm

--- main.tf
+++ main.tf
@@ -84,49 +84,49 @@ resource "azurerm_network_security_group" "nsg-test" {
   resource_group_name = azurerm_resource_group.rg-test.name
 }
 
-resource "azurerm_network_interface" "vmnic-test" {
-  location            = azurerm_resource_group.rg-test.location
-  name                = "vmtestnic1"
-  resource_group_name = azurerm_resource_group.rg-test.name
-  ip_configuration {
-    name                          = "ipconfig1"
-    private_ip_address_allocation = "Dynamic"
-    public_ip_address_id          = azurerm_public_ip.pip-test.id
-    subnet_id                     = azurerm_subnet.subnet-test.id
-  }
-  depends_on = [
-    azurerm_public_ip.pip-test,
-    azurerm_subnet.subnet-test,
-  ]
-}
+# resource "azurerm_network_interface" "vmnic-test" {
+#   location            = azurerm_resource_group.rg-test.location
+#   name                = "vmtestnic1"
+#   resource_group_name = azurerm_resource_group.rg-test.name
+#   ip_configuration {
+#     name                          = "ipconfig1"
+#     private_ip_address_allocation = "Dynamic"
+#     public_ip_address_id          = azurerm_public_ip.pip-test.id
+#     subnet_id                     = azurerm_subnet.subnet-test.id
+#   }
+#   depends_on = [
+#     azurerm_public_ip.pip-test,
+#     azurerm_subnet.subnet-test,
+#   ]
+# }
 
-resource "azurerm_linux_virtual_machine" "vm-test" {
-  admin_password                  = "!@Pa55w0rd123"
-  admin_username                  = "kmiszel"
-  disable_password_authentication = false
-  location                        = azurerm_resource_group.rg-test.location
-  name                            = "vmtest"
-  network_interface_ids           = [azurerm_network_interface.vmnic-test.id]
-  resource_group_name             = azurerm_resource_group.rg-test.name
-  secure_boot_enabled             = true
-  size                            = "Standard_B1ms"
-  vtpm_enabled                    = true
-  additional_capabilities {
-  }
-  os_disk {
-    caching              = "ReadWrite"
-    storage_account_type = "Standard_LRS"
-  }
-  source_image_reference {
-    offer     = "0001-com-ubuntu-server-focal"
-    publisher = "canonical"
-    sku       = "20_04-lts-gen2"
-    version   = "latest"
-  }
-  depends_on = [
-    azurerm_network_interface.vmnic-test,
-  ]
-}
+# resource "azurerm_linux_virtual_machine" "vm-test" {
+#   admin_password                  = "!@Pa55w0rd123"
+#   admin_username                  = "kmiszel"
+#   disable_password_authentication = false
+#   location                        = azurerm_resource_group.rg-test.location
+#   name                            = "vmtest"
+#   network_interface_ids           = [azurerm_network_interface.vmnic-test.id]
+#   resource_group_name             = azurerm_resource_group.rg-test.name
+#   secure_boot_enabled             = true
+#   size                            = "Standard_B1ms"
+#   vtpm_enabled                    = true
+#   additional_capabilities {
+#   }
+#   os_disk {
+#     caching              = "ReadWrite"
+#     storage_account_type = "Standard_LRS"
+#   }
+#   source_image_reference {
+#     offer     = "0001-com-ubuntu-server-focal"
+#     publisher = "canonical"
+#     sku       = "20_04-lts-gen2"
+#     version   = "latest"
+#   }
+#   depends_on = [
+#     azurerm_network_interface.vmnic-test,
+#   ]
+# }
 
 
 # resource "azurerm_network_interface_security_group_association" "res-3" {
