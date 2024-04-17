c075ee1e5223d3fb173694a36f1ce75f57e41863
Kamil Miszel <kamil.miszel@nordcloud.com>
2024-04-17 11:57:46 +0200

    usemodules

--- main.tf
+++ main.tf
@@ -10,3 +10,17 @@ module "storage_account_sa1" {
   resource_group_name = var.resource_group_names["rg1"]
   location            = var.location
 }
+
+module "resource_group_rg2" {
+  source              = "./rg"
+  resource_group_name = var.resource_group_names["rg2"]
+  location            = var.location
+}
+
+module "storage_account_sa2" {
+  source              = "./sa"
+  sa_name             = var.sa_name
+  resource_group_name = var.resource_group_names["rg2"]
+  location            = var.location
+  depends_on          = [module.resource_group_rg2]
+}
--- terraform.tfvars
+++ terraform.tfvars
@@ -1,5 +1,7 @@
 location = "northeurope"
 resource_group_names = {
   rg1 = "rg-modules"
+  rg2 = "rg-modules2"
 }
-sa_name = "kmiszelmodule187"
+sa_name  = "kmiszelmodule187"
+sa_name2 = "kmiszelmodule188"
