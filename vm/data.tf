562bc71e70af635ff83ad391ecd7513f0ca22bde
Kamil Miszel <kamil.miszel@nordcloud.com>
2024-04-17 13:42:26 +0200

    trynow

--- vm/data.tf
+++ vm/data.tf
@@ -1,5 +1,5 @@
 data "azurerm_subnet" "existing_subnet" {
-  name                 = var.subnet_name
-  virtual_network_name = var.vnet_name
-  resource_group_name  = var.vnet_resource_group_name
+  name                 = module.vnet_module.subnet_name
+  virtual_network_name = module.vnet_module.vnet_name
+  resource_group_name  = module.resource_group_rg1.resource_group_name
 }
