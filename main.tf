1b640f2f83a8f51aebe8927a11ce14acca04c8f2
K.Miszel <k.miszel@protonmail.com>
2024-10-15 14:11:39 +0200

    aci testing

--- main.tf
+++ main.tf
@@ -1,11 +1,11 @@
-resource "azurerm_resource_group" "example" {
-  name     = "example-RG"
-  location = "North Europe"
+# resource "azurerm_resource_group" "example" {
+#   name     = "example-RG"
+#   location = "North Europe"
 
-  # Tags for the Resource Group
-  tags = {
-    Environment = "Dev"
-    Owner       = "kmiszel"
-    Source      = "terraform"
-  }
-}
+#   # Tags for the Resource Group
+#   tags = {
+#     Environment = "Dev"
+#     Owner       = "kmiszel"
+#     Source      = "terraform"
+#   }
+# }
