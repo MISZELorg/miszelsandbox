59d89b68b6752daab80241df6176dbace0dc12ec
K.Miszel <k.miszel@protonmail.com>
2024-10-22 12:53:10 +0200

    add_cluster

--- data.tf
+++ data.tf
@@ -1,5 +1,5 @@
 data "azurerm_client_config" "aks_subscription" {}
 
-data "azuread_group" "k8s_admins" {
-  display_name = "k8s_admins"
-}
+# data "azuread_group" "k8s_admins" {
+#   display_name = "k8s_admins"
+# }
