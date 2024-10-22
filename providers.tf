0a7c2caa9b77b3abee1902e998c4b8eabfa9dc5b
K.Miszel <k.miszel@protonmail.com>
2024-10-22 13:11:40 +0200

    no oidc

--- providers.tf
+++ providers.tf
@@ -11,15 +11,15 @@ terraform {
     storage_account_name = "miszelsandbox"
     container_name       = "tfstate"
     key                  = "terraform.tfstate"
-    use_oidc             = true
-    subscription_id      = "f80611eb-0851-4373-b7a3-f272906843c4"
-    tenant_id            = "48c383d8-47c5-48f9-9e8b-afe4f2519054"
+    # use_oidc             = true
+    subscription_id = "f80611eb-0851-4373-b7a3-f272906843c4"
+    tenant_id       = "48c383d8-47c5-48f9-9e8b-afe4f2519054"
   }
 }
 
 provider "azurerm" {
   features {}
-  use_oidc = true
+  # use_oidc = true
 }
 
 provider "azuread" {
