6d914d030da9f9ea6353a515f8f65e535785f385
K.Miszel <k.miszel@protonmail.com>
2024-10-23 13:38:40 +0200

    resource_provider_registrations

--- providers.tf
+++ providers.tf
@@ -17,9 +17,14 @@ terraform {
   }
 }
 
+
 provider "azurerm" {
   features {}
-  use_oidc = true
+  # use_oidc = true
+
+  resource_provider_registrations = {
+    enabled = true
+  }
 }
 
 provider "azuread" {
