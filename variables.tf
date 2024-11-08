d76298031e19ad137f93cfb54bb0b3e4cee1764b
K.Miszel <k.miszel@protonmail.com>
2024-11-08 09:25:35 +0100

    pubaks

--- variables.tf
+++ variables.tf
@@ -3,63 +3,63 @@ variable "tenant_id" {
   type        = string
 }
 
-# variable "location" {
-#   description = "Location of the AKS cluster"
-#   type        = string
-# }
+variable "location" {
+  description = "Location of the AKS cluster"
+  type        = string
+}
 
-# variable "cluster_name" {
-#   description = "Name of the AKS cluster"
-#   type        = string
-# }
+variable "cluster_name" {
+  description = "Name of the AKS cluster"
+  type        = string
+}
 
-# variable "aks_tags" {
-#   description = "Common tags to apply to all AKS resources"
-#   type        = map(string)
-# }
+variable "aks_tags" {
+  description = "Common tags to apply to all AKS resources"
+  type        = map(string)
+}
 
-# variable "maintenance_window" {
-#   description = "Map of maintenance window configuration"
-#   type        = map(string)
-# }
+variable "maintenance_window" {
+  description = "Map of maintenance window configuration"
+  type        = map(string)
+}
 
-# variable "network_profile" {
-#   description = "Map of settings for the network profile"
-#   type        = map(string)
-# }
+variable "network_profile" {
+  description = "Map of settings for the network profile"
+  type        = map(string)
+}
 
-# variable "default_node_pool" {
-#   description = "Settings for the default node pool"
-#   type = object({
-#     name                          = string
-#     vm_size                       = string
-#     auto_scaling_enabled          = bool
-#     zones                         = list(string)
-#     node_count                    = number
-#     min_count                     = number
-#     max_count                     = number
-#     max_pods                      = number
-#     temporary_name_for_rotation   = string
-#     drain_timeout_in_minutes      = number
-#     max_surge                     = string
-#     node_soak_duration_in_minutes = number
-#   })
-# }
+variable "default_node_pool" {
+  description = "Settings for the default node pool"
+  type = object({
+    name                          = string
+    vm_size                       = string
+    auto_scaling_enabled          = bool
+    zones                         = list(string)
+    node_count                    = number
+    min_count                     = number
+    max_count                     = number
+    max_pods                      = number
+    temporary_name_for_rotation   = string
+    drain_timeout_in_minutes      = number
+    max_surge                     = string
+    node_soak_duration_in_minutes = number
+  })
+}
 
-# variable "node_pool_config" {
-#   description = "Node pool configuration for AKS"
-#   type = map(object({
-#     name                 = string
-#     vm_size              = string
-#     auto_scaling_enabled = bool
-#     zones                = list(string)
-#     node_count           = number
-#     min_count            = number
-#     max_count            = number
-#     max_pods             = number
-#     mode                 = string
-#   }))
-# }
+variable "node_pool_config" {
+  description = "Node pool configuration for AKS"
+  type = map(object({
+    name                 = string
+    vm_size              = string
+    auto_scaling_enabled = bool
+    zones                = list(string)
+    node_count           = number
+    min_count            = number
+    max_count            = number
+    max_pods             = number
+    mode                 = string
+  }))
+}
 
 # variable "grafana_name" {
 #   description = "Name of the Grafana instance"
